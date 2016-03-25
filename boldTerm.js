73f62391035e280aea95ef082c4816f0bfe4db2c
dual1tyx <dual1tyx@hotmail.co.uk>
2016-03-25 21:08:18 +0000

    Modified method, added package

--- boldTerm.js
+++ boldTerm.js
@@ -1,5 +1,5 @@
 module.exports = boldterm
 
-function updateString(input, term) {
+function boldterm(input, term) {
     return input.replace(new RegExp('(^|\)(' + term + ')(\|$)','ig'), '$1<strong>$2</strong>$3');
 }
